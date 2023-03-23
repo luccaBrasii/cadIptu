2aaa82be945b9f39ee66ca44200b51c4a9e23724
Lucca Lopes Rodrigues <m1515080@pmg.goiania.go.gov.br>
2023-03-23 15:59:11 -0300

    second

--- js/functions.js
+++ js/functions.js
@@ -1,116 +1,49 @@
-$(()=>{
-
 //MOSTRAR OS ANEXOS - JEITO AUTOMATICO COM POO
 
 //OBJETO COM UMA KEY QUE EXECUTA UMA FUNÇÃO ESPECÍFICA
 tabela = {
-    um:function(){$('.info-1').fadeIn()},
-    dois:function(){$('.info-2').fadeIn()},
-    tres:function(){$('.info-3').fadeIn()},
-    quatro:function(){$('.info-4').fadeIn()},
-    cinco:function(){$('.info-5').fadeIn()},
-    seis:function(){$('.info-6').fadeIn()},
-    sete:function(){$('.info-7').fadeIn()},
-    oito:function(){$('.info-8').fadeIn()}
+    um: function () { $('.info-1').fadeToggle() },
+    dois: function () { $('.info-2').fadeToggle() },
+    tres: function () { $('.info-3').fadeToggle() },
+    quatro: function () { $('.info-4').fadeToggle() },
+    cinco: function () { $('.info-5').fadeToggle() },
+    seis: function () { $('.info-6').fadeToggle() },
+    sete: function () { $('.info-7').fadeToggle() },
+    oito: function () { $('.info-8').fadeToggle() }
 }
+
+//FECHAR DIVS
 
 
 //PEGA TODOS 'LINKS' DOS 'TÍTULOS' DO ANEXO
 table = $('.table a')
 
 //PERCORRE TODOS OS LINKS 
-$(table).each(function() {
+$(table).each(function () {
     //DEPOIS QUE O MOUSE PASSA EM CIMA DO TÍTULO
-    $(this).hover(()=>{
-        
+    $(this).click(() => {
+
         //CRIA UMA VARIAVEL COM O ATRIBUTO 'NAME' DO TÍTULO
         var x = $(this).attr('name')
-        console.log(x)
 
         //CRIA UMA VARIAVEL COM O OBJETO TABELA EM QUE A 'KEY' É IGUAL AO 'NAME' DO TÍTULO E EXECUTA A FUNÇÃO EM ESPECÍFICO
         teste = tabela[x]
         teste()
-    },()=>{
-        //OCULTA TODAS AS INFO DEPOIS DO MOUSE SAIR
-        $('.info').fadeOut()})
-  });
- 
 
+    })
+});
 
 //ANEXO 9 - INFORMAÇÕES SOBRE A EDIFICAÇÃO - obs: esse tive que fazer fora do objeto pois ele não é um link
-$('.botao-info9').hover(()=>{
+$('.botao-info9').click((e) => {
+    e.stopPropagation()
+    $('.info-9').fadeToggle();
+});
 
-    $('.info-9').fadeIn()
-},()=>{
-$('.info-9').fadeOut()})
-
-
-//MOSTRAR OS ANEXOS - MANUALMENTE
-
-
-/*
-//ANEXO 1
-$('.botao-info1').hover(()=>{
-
-    $('.info-1').fadeIn()
-},()=>{
-$('.info-1').fadeOut()})
-
-
-//ANEXO 2
-$('.botao-info2').hover(()=>{
-
-    $('.info-2').fadeIn()
-},()=>{
-$('.info-2').fadeOut()})
-
-//ANEXO 3
-$('.botao-info3').hover(()=>{
-
-    $('.info-3').fadeIn()
-},()=>{
-$('.info-3').fadeOut()})
-
-//ANEXO 4
-$('.botao-info4').hover(()=>{
-
-    $('.info-4').fadeIn()
-},()=>{
-$('.info-4').fadeOut()})
-
-//ANEXO 5
-$('.botao-info5').hover(()=>{
-
-    $('.info-5').fadeIn()
-},()=>{
-$('.info-5').fadeOut()})
-
-//ANEXO 6
-$('.botao-info6').hover(()=>{
-
-    $('.info-6').fadeIn()
-},()=>{
-$('.info-6').fadeOut()})
-
-//ANEXO 7
-$('.botao-info7').hover(()=>{
-
-    $('.info-7').fadeIn()
-},()=>{
-$('.info-7').fadeOut()})
-
-//ANEXO 8
-$('.botao-info8').hover(()=>{
-
-    $('.info-8').fadeIn()
-},()=>{
-$('.info-8').fadeOut()})
-
-
-*/
-
+$('body').click(() => {
+    $('.info').fadeOut()
 })
 
-
-
-
+//PARA NAO FECHAR A JANELA QUANDO CLICAR NO 'i'
+table.click((e) => {
+    e.stopPropagation()
+})
